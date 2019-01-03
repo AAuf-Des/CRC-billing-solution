2035529f32adc7887b3dadb85de226bff9dc2413
sebastian Persson <sebmail99@gmail.com>
2019-01-03 20:33:46 +0100

    fixed so that all months on Cdr get transfered to Invoice object

--- class.h
+++ class.h
@@ -4,41 +4,9 @@
 #include <string>
 #include <fstream>
 #include <ctime>
+#include <vector>
 using namespace std;
 
-
-struct dateStr{
-    public:
-    int day;
-    int month;
-    int year;
-    time_t epochTime;
-
-    void epochToDate()
-    {
-        struct tm localTime;
-        localTime = *localtime(&epochTime);
-        day = localTime.tm_mday;
-        month = localTime.tm_mon + 1;
-        year = localTime.tm_year + 1900;
-    }
-
-    //used to asign dates
-    void assignDate(struct tm readTime, int x){
-        day = 1;
-        month = readTime.tm_mon + x;
-        year = readTime.tm_year + 1900;
-        if (month <= 0){
-            month = month + 12;
-            year = year - 1;
-        }
-
-    }
-
-    void printDate(){
-        cout << year << "-" << month << "-" << day << endl;
-    }
-};
 
 
 //invoice class
@@ -47,39 +15,54 @@ class invoice{
     long long int number;
 
     public:
-    dateStr lastDates[3];
-    int yearMonthValue[3];
-    int totalDurationPerMonth[3] = {0,0,0};
+    vector<struct tm> invoiceDates;
+    vector<int> totalDurationPerMonth;
+    vector<int> yearMonthValue;
     
-    void getNumber(long long int x);
-    void printInfo();
+    void setNumber(long long int x);
+    void setAmountOfMonths(int amountOfMonths);
     void getYearMonthValue();
+    void printInfo();
+    void printSize(){
+        cout << invoiceDates.size() << endl;
+    }
 
     long long int copyNumber();
+
+
 };
 
+void invoice::setAmountOfMonths(int amountOfMonths){
+    invoiceDates.resize(amountOfMonths);
+    totalDurationPerMonth.resize(amountOfMonths);
+    yearMonthValue.resize(amountOfMonths);
+}
+
+
 void invoice::getYearMonthValue(){
-    yearMonthValue[0] = (lastDates[0].year * 100) + lastDates[0].month;
-    yearMonthValue[1] = (lastDates[1].year * 100) + lastDates[1].month;
-    yearMonthValue[2] = (lastDates[2].year * 100) + lastDates[2].month;
+    cout << "hello" << endl; // fix
 }
 
-void invoice::getNumber(long long int x){
+void invoice::setNumber(long long int x){
     number = x;
-}
-
-void invoice::printInfo(){
-    cout << "MSISDN: " << number << endl;
-    cout << yearMonthValue[0] << ": " << totalDurationPerMonth[0] << endl;
-    cout << yearMonthValue[1] << ": " << totalDurationPerMonth[1] << endl;
-    cout << yearMonthValue[2] << ": " << totalDurationPerMonth[2] << endl;
-    cout << endl;
-    cout << "________________________" << endl;
 }
 
 long long int invoice::copyNumber(){
     return number;
 }
+
+void invoice::printInfo(){
+    cout << "MSIDN: " << number << endl;
+    for (int i = 0; i < invoiceDates.size(); i++){
+        cout << "date # " << i << " " << invoiceDates[i].tm_year << "-" << invoiceDates[i].tm_mon << "-" << invoiceDates[i].tm_mday << endl;
+    }
+    cout << "__________________________" << endl;
+}
+
+
+
+
+
 
 
 //phonecall class
@@ -88,31 +71,26 @@ class phoneCall{
     long long int reciver;
     long long int caller;
     time_t epochTime;
-    dateStr date;
+    struct tm date;
     int duration;
     
 
     public:
-    long long int pCaller;
+    long long int publicNumber;     //fix this, needed for lamba function
     int yearMonthValue;
     void getData(string file, int index);
     void printInfo();
-    void epochToDate();
     void getYearMonthValue();
+    void epochToDate();
     
 
     //copy functions, return caller, duration etc etc..
     long long int copyCaller();
     int copyDuration();
     int copyEpochTime();
-    int copyMonth();
-    int copyYear();
 };
 
 
-void phoneCall::getYearMonthValue(){
-    yearMonthValue = (date.year * 100) + date.month;
-}
 
 void phoneCall::getData(string file, int index){
     int count = 0;
@@ -128,33 +106,33 @@ void phoneCall::getData(string file, int index){
 
     myFile >> caller >> c >> reciver >> c >> epochTime >> c >> duration >> c;
 
-    pCaller = caller;
-
+    publicNumber = caller;
     epochToDate();
     getYearMonthValue();
     myFile.close();
+}
 
+void phoneCall::epochToDate(){
+    date = *gmtime(&epochTime);
+
+    date.tm_mon += 1;
+    date.tm_year += 1900;
+}
+
+void phoneCall::getYearMonthValue(){
+    yearMonthValue = (date.tm_year * 100) + date.tm_mon;
 }
 
 void phoneCall::printInfo(){
     cout << "caller: " << caller << endl;
     cout << "reciver: " << reciver << endl;
     cout << "call duration: " << duration << endl;
-    cout << "date: " << date.year << "-" << date.month << "-" << date.day << endl;
+    cout << "date: " << date.tm_year << "-" << date.tm_mon << "-" << date.tm_mday << endl;
     cout << "YYYYMM int format: " << yearMonthValue << endl;
     cout << "_____________________________________________________________" << endl;
 
 }
 
-void phoneCall::epochToDate(){
-    struct tm localTime;
-    localTime = *localtime(&epochTime);
-
-
-    date.day = localTime.tm_mday;
-    date.month = localTime.tm_mon + 1;
-    date.year = localTime.tm_year + 1900;
-}
 
 long long int phoneCall::copyCaller(){
     return caller;
@@ -168,12 +146,5 @@ int phoneCall::copyEpochTime(){
     return epochTime;
 }
 
-int phoneCall::copyMonth(){
-    return date.month;
-}
-    
-int phoneCall::copyYear(){
-    return date.year;
-}
 
 #endif
--- main.cpp
+++ main.cpp
@@ -8,15 +8,21 @@
 
 using namespace std;
 
-const string file = "CdrTot.txt";
+const string file = "CdrMonth.txt";
 
 int countLines(string file);
 
 int countAmountOfCallers(vector<phoneCall> call, int amountOfLines);
 
-void assignNumbersToInvoices(vector<phoneCall> call, invoice invoiceArray[], int amountOfLines);
+void assignNumbersToInvoices(vector<phoneCall> call, vector<invoice> &invoiceArray, int amountOfLines);
+
+int calculateAmountOfMonths(vector<phoneCall> call, int linesAmount);
 
 time_t getLastDate(vector<phoneCall> call, int amountOfLines);
+
+time_t getFirstDate(vector<phoneCall> call, int amountOfLines);
+
+void setInvoiceMonths(vector<invoice> &invoiceArray, vector<phoneCall> call, int amountOfMonths, int amountOfLines, int amountOfCallers);
 
 
 
@@ -31,7 +37,7 @@ int main()
 
     sort(call.begin(), call.end(), [ ]( const auto& left, const auto& right )   //sort phonecalls by phoneNumber, lowest to highest.
     {
-        return left.pCaller < right.pCaller;
+        return left.publicNumber < right.publicNumber;
     });
 
     for (int i = 0; i < linesAmount; i++){              //print all phonecalls, delete before relese
@@ -40,19 +46,30 @@ int main()
 
     system("pause");
 
+
     int amountOfCallers = countAmountOfCallers(call, linesAmount);
-    invoice *invoiceArray = new invoice[amountOfCallers];
+    vector <invoice> invoiceArray(amountOfCallers);
 
-    assignNumbersToInvoices(call, invoiceArray, linesAmount);
+    assignNumbersToInvoices(call, invoiceArray, linesAmount);               //assigning all MSISN numbers to the invoice objects
 
-    dateStr lastDatesInCdr[3];
-    time_t epochTime = getLastDate(call, linesAmount);
-    struct tm readTime;
-    readTime = *localtime(&epochTime);
-    lastDatesInCdr[0].assignDate(readTime, 1);
-    lastDatesInCdr[1].assignDate(readTime, 0);
-    lastDatesInCdr[2].assignDate(readTime, -1);
+    int amountOfMonths = calculateAmountOfMonths(call, linesAmount);
 
+    cout << "amountOfMonths: " << amountOfMonths << endl;
+
+    setInvoiceMonths(invoiceArray, call, amountOfMonths, linesAmount, amountOfCallers);
+
+    for (int i = 0; i < amountOfCallers; i++){
+        invoiceArray[i].printInfo();
+    }
+
+
+
+
+
+    system("pause");
+
+
+    /*
     for (int i = 0; i < amountOfCallers; i++){
         for (int j = 0; j < 3; j++){
             invoiceArray[i].lastDates[j] = lastDatesInCdr[j];
@@ -76,6 +93,7 @@ int main()
     for (int i = 0; i < amountOfCallers; i++){                      //print all invoice info,delete before release
         invoiceArray[i].printInfo();
     }
+    */
 
     return 0;
 }
@@ -106,14 +124,29 @@ int countAmountOfCallers(vector<phoneCall> call, int amountOfLines){
     return count;
 }
 
-void assignNumbersToInvoices(vector<phoneCall> call, invoice invoiceArray[], int amountOfLines){
+void assignNumbersToInvoices(vector<phoneCall> call, vector<invoice> &invoiceArray, int amountOfLines){
     int invoiceNumber = 0;
     for (int i = 0; i < amountOfLines; i++){
         if (call[i].copyCaller() != call[i+1].copyCaller()){
-            invoiceArray[invoiceNumber].getNumber(call[i].copyCaller());
+            invoiceArray[invoiceNumber].setNumber(call[i].copyCaller());
             invoiceNumber++;
         }
     }
+}
+
+int calculateAmountOfMonths(vector<phoneCall> call, int linesAmount){
+    time_t lastEpochDateInCdr = getLastDate(call, linesAmount);
+    time_t firstEpochDateInCdr = getFirstDate(call, linesAmount);
+    struct tm firstTmDateInCdr = *gmtime(&firstEpochDateInCdr);
+    struct tm lastTmDateInCdr = *gmtime(&lastEpochDateInCdr);
+
+    int yearsAppart = lastTmDateInCdr.tm_year - firstTmDateInCdr.tm_year;
+    int monthsAppart = lastTmDateInCdr.tm_mon - firstTmDateInCdr.tm_mon ;
+
+    int totalMonths = (yearsAppart * 12) + monthsAppart + 1;
+
+
+    return totalMonths;
 }
 
 
@@ -125,5 +158,39 @@ time_t getLastDate(vector<phoneCall> call, int amountOfLines){
         }
     }
     return lastDate;
+}
+
+time_t getFirstDate(vector<phoneCall> call, int amountOfLines){
+    int firstDate = call[0].copyEpochTime();
+    for (int i = 0; i < amountOfLines; i++){
+        if (firstDate > call[i].copyEpochTime()){
+            firstDate = call[i].copyEpochTime();
+        }
+    }
+    return firstDate;
+}
+
+
+void setInvoiceMonths(vector<invoice> &invoiceArray, vector<phoneCall> call, int amountOfMonths, int amountOfLines, int amountOfCallers){
+    time_t first = getFirstDate(call, amountOfLines);
+    time_t last = getLastDate(call, amountOfLines);
+    long long int diff = difftime(last, first);
+    time_t oneMonth = 2628000;
+
+    time_t tempMonth = first;
+
+    for (int invoiceIteration = 0; invoiceIteration < amountOfCallers; invoiceIteration++){
+        invoiceArray[invoiceIteration].setAmountOfMonths(amountOfMonths);
+
+        for(int monthIteration = 0; monthIteration < amountOfMonths; monthIteration++){
+            invoiceArray[invoiceIteration].invoiceDates[monthIteration] = *gmtime(&tempMonth);
+
+            invoiceArray[invoiceIteration].invoiceDates[monthIteration].tm_year += 1900;
+            invoiceArray[invoiceIteration].invoiceDates[monthIteration].tm_mon += 1;
+            //invoiceArray[invoiceIteration].invoiceDates[monthIteration].tm_mday = 15;
+            tempMonth += oneMonth;
+        }
+        tempMonth = first;
+    }
 }
 
